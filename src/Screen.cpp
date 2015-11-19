bd2ef6056388590fd5e1a69bb8922e2bdcc5feb4
Tom Barrett <spalf0@gmail.com>
2015-11-19 17:40:12 -0600

    Update Screen.cpp

--- src/Screen.cpp
+++ src/Screen.cpp
@@ -13,7 +13,8 @@ Screen::Screen()
     start_color();
     init_pair(1, COLOR_GREEN, COLOR_BLACK);
     init_pair(2, COLOR_YELLOW,COLOR_BLACK);
-    init_pair(3, COLOR_WHITE, COLOR_BLUE);
+    init_pair(3, COLOR_WHITE, COLOR_WHITE);
+    init_pair(4, COLOR_WHITE, COLOR_BLUE);
 }
 
 Screen::~Screen()
